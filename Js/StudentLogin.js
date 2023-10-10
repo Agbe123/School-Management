8adcabe1f8122a4d137d7ffe4221e529bdbe4b42
Nelson877 <66440163+Nelson877@users.noreply.github.com>
2023-10-10 21:09:55 +0000

    the boy image update

--- Js/StudentLogin.js
+++ Js/StudentLogin.js
@@ -1,14 +1,32 @@
-function validateForm() {
-  let studentId = document.studentForm.studentId.value;
+// function validateForm() {
+//   let studentId = document.studentForm.studentId.value;
 
-  let pin = document.studentForm.pin.value;
+//   let pin = document.studentForm.pin.value;
 
-  if (studentId== null || studentId == " ") {
-    alert("Student ID can't be blank");
+//   if (studentId<10 || studentId == " ") {
+//     alert("Student ID can't be blank");
 
-    return false;
-  } else if (pin.length< 10) {
+//     return false;
+//   } else if (pin.length< 6) {
+//     alert("Pin must be at least 6 characters long.");
+//     return false;
+//   }else{
+
+//   }
+// }
+function validateForm(){
+    // event.preventDefault();
+
+
+    let studentId=document.studentForm.studentId.value;
+    let pin=document.studentForm.pin.value
+
+if (studentId==null||studentId=="") {
+    alert("StudentId can't be blank.");
+    window.location.replace("Admission.html")
+    return false
+}else if (pin.lenght<6) {
     alert("Pin must be at least 6 characters long.");
     return false;
-  }
+}
 }
